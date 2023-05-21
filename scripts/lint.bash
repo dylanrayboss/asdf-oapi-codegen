e57113816997c59835ae08d235d139898cb4b54c
dylan <dylanrayboss@gmail.com>
2023-05-21 09:40:54 -0500

    fixes: merge conflicts

--- scripts/lint.bash
+++ scripts/lint.bash
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
 shellcheck --shell=bash --external-sources \
 	bin/* --source-path=template/lib/ \
 	lib/* \
@@ -8,22 +7,3 @@ shellcheck --shell=bash --external-sources \
 
 shfmt --language-dialect bash --diff \
 	./**/*
-=======
-# lint this repo
-shellcheck --shell=bash --external-sources \
-	setup.bash \
-	scripts/*
-
-shfmt --language-dialect bash --diff \
-	setup.bash \
-	scripts/*
-
-# lint the template/
-shellcheck --shell=bash --external-sources \
-	template/bin/* --source-path=template/lib/ \
-	template/lib/* \
-	template/scripts/*
-
-shfmt --language-dialect bash --diff \
-	template/**/*
->>>>>>> f31da0c3fc5a28058f20e5ec6fb81e225e70b1eb
