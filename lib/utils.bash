934681f732c46c296351e8394badcfffcd5760a8
dylan <dylanrayboss@gmail.com>
2023-05-21 10:12:35 -0500

    adds: go install used in asdf-goconvey

--- lib/utils.bash
+++ lib/utils.bash
@@ -49,26 +49,47 @@ download_release() {
 }
 
 install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="${3%/bin}/bin"
+  local install_type="$1"
+  local version="$2"
+  local install_path="${3%/bin}/bin"
 
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
-	fi
+  if [ "$install_type" = "version" ]; then
+    version="v$version"
+  fi
 
-	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+  (
+    asdf current golang >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+      echo "An asdf Go version has already been selected; I'll use that to build $TOOL_NAME."
+    else
+      echo "No asdf Go version is selected; I'll install the latest version to build $TOOL_NAME."
 
-		# TODO: Assert oapi-codegen executable exists.
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+      # In order to build a Go application from source, we need a Go compiler.
+      asdf plugin add golang || true
+      asdf install golang latest
 
-		echo "$TOOL_NAME $version installation was successful!"
-	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
-	)
+      # We can't use `asdf shell` within an `asdf plugin test` environment. For
+      # compatibility with both `asdf plugin test` and `asdf install`, we'll
+      # configure the Go version with `asdf local`; and to avoid making unwanted
+      # modifications to .tool-versions in the pwd, we'll move somewhere else.
+      mkdir -p "$install_path"
+      cd "$install_path"
+      asdf local golang latest
+    fi
+
+    # Now we can let the Go toolchain retrieve and install the application as
+    # normal, except that we'll dump the binary into the install path instead
+    # of $GOPATH/bin.
+    export GOBIN="$install_path"
+    go install "$GO_PACKAGE"@"$version" || fail "An error occurred while installing $TOOL_NAME $version."
+
+    local tool_cmd
+    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+
+    echo "$TOOL_NAME $version installation was successful!"
+  ) || (
+    rm -rf "$install_path"
+    fail "An error occurred while installing $TOOL_NAME $version."
+  )
 }
